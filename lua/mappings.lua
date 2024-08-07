8006c441554b597cc7bc71a0c09e8454a1fcd766
entangle2giraffe <bittersalad@tutanota.de>
2024-08-07 18:41:36 +0700

    NvChad Starter

---
+++ lua/mappings.lua
@@ -0,0 +1,12 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<leader>rn", ":set relativenumber!<CR>", { desc = "Toggles relativenumber" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
